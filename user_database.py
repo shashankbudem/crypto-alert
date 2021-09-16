4ae4d49983d4e18a236e6c12a8d7ec8010af6167
shashankbudem <59564927+shashankbudem@users.noreply.github.com>
2021-09-16 15:55:48 +0530

    Delete user_database.py

--- user_database.py
+++
@@ -1,7 +0,0 @@
-# users_pass = {"user1": "pass1", "user2": "pass2"}
-# users_pass = {}
-# def add(username,password):
-#     if username not in list(users_pass.keys()):
-#         users_pass[username] = password
-
-
