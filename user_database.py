70850851227d95153c86ec018b2b2e16f7b095c6
shashankbudem <shashankbudem@gmail.com>
2021-09-16 15:52:16 +0530

    initial

---
+++ user_database.py
@@ -0,0 +1,7 @@
+# users_pass = {"user1": "pass1", "user2": "pass2"}
+# users_pass = {}
+# def add(username,password):
+#     if username not in list(users_pass.keys()):
+#         users_pass[username] = password
+
+
